c663a0e500e787c604a5eef4febd790530f66741
kiboma <omayioben@gmail.com>
2023-11-29 17:13:25 +0300

    Add props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Logo from '../assets/logo.png'
 import './header.css'
 
-export default function Header() {
+export default function Header(props) {
   return (
     <div className='header'>
-        <h1>Integer Counter</h1>
+        <h1>{props.title}</h1>
         <img className='logo' src={Logo} alt="logo" />
     </div>
   )
