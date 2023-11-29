23df7d7ebfe8f00b2fb118327b37d5caa6f1473b
kiboma <omayioben@gmail.com>
2023-11-29 10:03:48 +0300

    Add functions for addition, subtraction and reset

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount]=useState(0)
+  
+  function handleAdd(){
+    setCount(count+1)
+  }
+
+  function handleSub(){
+    setCount(count-1)
+  }
+  function handleReset(){
+    setCount(0)
+  }
   return (
     <div className="App">
       <>
       <Header />
       <hr />
       <div className='counter'>
-        <button>Subtract</button>
-        <h2>0</h2>
-        <button>Add</button>
+        <button onClick={handleSub}>Subtract</button>
+        <h2>{count}</h2>
+        <button onClick={handleAdd}>Add</button>
       </div>
       <hr />
       <div className='center-reset'>
-        <button className='reset'>Reset Counter</button>
+        <button onClick={handleReset} className='reset'>Reset Counter</button>
       </div>
      <hr />
 
