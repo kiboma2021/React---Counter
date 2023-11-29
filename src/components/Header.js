7925911bb065f66b77fdeb8234a25f99102df9ff
kiboma <omayioben@gmail.com>
2023-11-29 09:38:20 +0300

    Add components folder for various components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <h1>APA insurance</h1>
+        <img className='logo' src={Logo} alt="logo" />
+    </div>
+  )
+}
